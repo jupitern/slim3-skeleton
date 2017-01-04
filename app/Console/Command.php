97757c0dbd339dbd9711b4f3afc8b9bdbb927e51
Nuno Chaves <Nuno Chaves@ServDebt.local>
2017-01-04 12:31:01 +0000

    added variables type hinting

--- app/Console/Command.php
+++ app/Console/Command.php
@@ -5,8 +5,12 @@ namespace App\Console;
 
 Abstract class Command
 {
+
+	/** @var \Psr\Http\Message\ServerRequestInterface */
 	public $request;
+	/** @var \Psr\Http\Message\ResponseInterface */
 	public $response;
+	/** @var \Psr\Log\LoggerInterface */
 	public $logger;
 
 	public function __construct($request, $response, $logger = null)
